66afff56f4d6c25186181c52c633b36aa1816327
Grigory Tomchuk <grigorijtomczuk@gmail.com>
2026-03-23 05:47:58 +0300

    Rebranding

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,5 +1,5 @@
-export const MODULE_ID = "send-in-game-image-to-chat";
-export const MODULE_NAME = "Send In-Game Image To Chat";
+export const MODULE_ID = "chat-image";
+export const MODULE_NAME = "Chat Image";
 export const MODULE_NAMESPACE = "sendInGameImageToChat";
 
 export const CHAT_MESSAGE_IMAGE_CLASS = MODULE_ID + "-image";
