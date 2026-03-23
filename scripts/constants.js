b549ab72839d653348e7a2816035a510e26e39fc
Grigory Tomchuk <grigorijtomczuk@gmail.com>
2026-03-23 23:08:18 +0300

    Missed a namespace change

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,5 +1,5 @@
 export const MODULE_ID = "chat-image";
 export const MODULE_NAME = "Chat Image";
-export const MODULE_NAMESPACE = "sendInGameImageToChat";
+export const MODULE_NAMESPACE = "chatImage";
 
 export const CHAT_MESSAGE_IMAGE_CLASS = MODULE_ID + "-image";
